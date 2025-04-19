eb9c8cc0179754aec17a4cbc2d1f6db456e5cd5f
Sheetal <sheetalbajajgodda@gmail.com>
2025-04-19 13:36:45 +0530

    added main page with child and parent category

---
+++ main.js
@@ -0,0 +1,8 @@
+function navigateTo(userType) {
+    if (userType === "parent") {
+      window.location.href = "parent.html";
+    } else if (userType === "child") {
+      window.location.href = "child.html";
+    }
+  }
+  
